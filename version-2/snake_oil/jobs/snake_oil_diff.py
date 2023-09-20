84305c7fb683c8aa49d2e46723307721447658fa
Øyvind Eide <eide.oyvind87@gmail.com>
2023-09-20 14:20:26 +0200

    Add storage with version 2

---
+++ version-2/snake_oil/jobs/snake_oil_diff.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from ecl.summary import EclSum
+
+
+def writeDiff(filename, vector1, vector2):
+    with open(filename, "w", encoding="utf-8") as f:
+        for node1, node2 in zip(vector1, vector2):
+            f.write(f"{node1-node2:f}\n")
+
+
+if __name__ == "__main__":
+    ecl_sum = EclSum("SNAKE_OIL_FIELD")
+
+    report_step = 199
+    writeDiff(
+        f"snake_oil_opr_diff_{report_step}.txt",
+        ecl_sum.numpy_vector("WOPR:OP1"),
+        ecl_sum.numpy_vector("WOPR:OP2"),
+    )
+    writeDiff(
+        f"snake_oil_wpr_diff_{report_step}.txt",
+        ecl_sum.numpy_vector("WWPR:OP1"),
+        ecl_sum.numpy_vector("WWPR:OP2"),
+    )
+    writeDiff(
+        f"snake_oil_gpr_diff_{report_step}.txt",
+        ecl_sum.numpy_vector("WGPR:OP1"),
+        ecl_sum.numpy_vector("WGPR:OP2"),
+    )
